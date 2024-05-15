16f78dcce39f8a5a0ad5812dc86ce7e5afc27af6
AJITHKUMARFULLSTACK <ajith124aj@gmail.com>
2024-05-15 10:11:20 +0530

    initial commite

---
+++ MVCProject/Controllers/EnrollmentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        public string value = "";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Datamodle e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                Datamodle er = new Datamodle();
+                using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-KGIIVHG\\BMSERVER;Integrated Security=true;Initial Catalog=Sample"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_EnrollDetail", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@FirstName", e.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", e.LastName);
+                        cmd.Parameters.AddWithValue("@Password", e.Password);
+                        cmd.Parameters.AddWithValue("@Gender", e.Gender);
+                        cmd.Parameters.AddWithValue("@Email", e.Email);
+                        cmd.Parameters.AddWithValue("@Phone", e.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@SecurityAnwser", e.SecurityAnwser);
+                        cmd.Parameters.AddWithValue("@status", "INSERT");
+                        con.Open();
+                        ViewData["result"] = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
